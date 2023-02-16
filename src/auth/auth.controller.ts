8f063a2c78df3b9bc39a52908585bb1c9d2b5b9b
David Siqueira <davidsiqueira.hog@gmail.com>
2023-02-15 22:44:16 -0300

    ajustes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,12 +6,11 @@ import {
   Get,
   Query,
   Headers,
-  HttpException,
+  Res,
   HttpStatus,
 } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtAuthGuard } from './authStrategy/jwt-auth.guard';
-
 import { LocalAuthGuard } from './authStrategy/local-auth.guard';
 
 @Controller()
@@ -25,13 +24,14 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @Get('auth/isvalid/')
-  async isLogged(@Query('userId') id, @Headers() headers) {
+  async isLogged(@Query('userId') id, @Res() res, @Headers() headers) {
     const userInfo = await this.usersService.validateToken(
       id,
       headers['authorization'].split(' ')[1],
     );
     if (!userInfo) {
-      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+      res.status(HttpStatus.UNAUTHORIZED);
+      return;
     }
     return userInfo;
   }
